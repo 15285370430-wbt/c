668810cf9c1be2c3a104613fb23b2f8151d831d0
15285370430-wbt <2473445825@qq.com>
2020-11-22 19:30:19 +0800

    一瓶引饮料一元，两个瓶子能换一个汽水，20元能喝多少汽水。用编程实现。

--- drink/drink/drink.c
+++ drink/drink/drink.c
@@ -1,16 +1,23 @@
 #include<stdio.h>
 
-int Drink(int m) {
-	int b = m;
-	if (m < 2) {
-		b = m;
+int Drink(int m, int p) {
+
+	int sum = 0;
+	int ret = 0;
+	int tem = 0;
+	sum = m / p;
+	ret = sum;
+	while (ret /2!= 0) {
+		tem = ret;
+		ret = ret / 2;
+		sum += ret;
+		ret = tem % 2 + ret;
+
 	}
-	if (m >= 2) {
-		b += b / 2;
-	}
-	return b;
+	return sum;
 }
+	
 int main() {
-	printf("%d", Drink(20));
+	printf("%d", Drink(20,1));
 	return 0;
 }
